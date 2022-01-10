034b1c9c7ef74cf20078a1e584fdf2dd74c06e54
jonyprill <jonathantprill@gmail.com>
2022-01-10 16:25:45 -0600

    added task input box and type selector

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+    var listItemEl = document.createElement("li");              //create new task item
+    listItemEl.className = "task-item";                         //Styles using CSS
+    listItemEl.textContent = "This is a new task.";             //Adds text
+    tasksToDoEl.appendChild(listItemEl);                        //Appends the element to the task list
 }
 
 
