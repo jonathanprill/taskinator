6ad8b0eb99f272965ca2a67913cd94416b5674bf
jonyprill <jonathantprill@gmail.com>
2022-01-11 09:40:44 -0600

    tasks added to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,19 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;   //the dot value is what retreives the input from the box
+    var taskTypeInput = document.querySelector("select[name='task-type']").value
 
     var listItemEl = document.createElement("li");              //create new task item
     listItemEl.className = "task-item";                         //Styles using CSS
-    listItemEl.textContent = "This is a new task.";             //Adds text
+ 
+    var taskInfoEl = document.createElement("div");             // create div to hold task info and add to list item
+    taskInfoEl.className = "task-info";                         // give it a class name
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";    // add HTML content to div
+
+    listItemEl.appendChild(taskInfoEl);
+    
+    // listItemEl.textContent = taskNameInput;                     //Adds text
     tasksToDoEl.appendChild(listItemEl);                        //Appends the element to the task list
 }
 
